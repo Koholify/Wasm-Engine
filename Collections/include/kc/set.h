d3b09c80b29fee7e9f9d3f14a4a5f33f4ab6c828
Koholify <kprice2121@gmail.com>
2023-02-09 19:30:09 -0400

    string set added

--- Collections/include/kc/set.h
+++ Collections/include/kc/set.h
@@ -15,7 +15,7 @@ typedef struct kc_set_item {
 
 typedef struct kc_strset_item {
 	bool is_used;
-	char item[16];
+	char item[32];
 } kc_strset_item;
 
 typedef struct kc_set {
@@ -27,20 +27,32 @@ typedef struct kc_strset {
 } kc_strset;
 
 kc_set kc_set_init(size_t len);
-kc_strset kc_sset_init(size_t len);
+kc_strset kc_strset_init(size_t len);
+
 void kc_set_free(kc_set t);
+void kc_strset_free(kc_strset t);
+
 size_t kc_set_len(kc_set t);
 size_t kc_set_cap(kc_set t);
+size_t kc_strset_len(kc_strset t);
+size_t kc_strset_cap(kc_strset t);
+
 int_hash kc_set_hash(kc_set t);
 str_hash kc_strset_hash(kc_strset t);
 
 bool kc_set_has(kc_set set, size_t val);
 void kc_set_set(kc_set * set, size_t val);
 void kc_set_remove(kc_set set, size_t val);
+bool kc_strset_has(kc_strset set, const char* val);
+void kc_strset_set(kc_strset * set, const char* val);
+void kc_strset_remove(kc_strset set, const char* val);
 
 kc_set kc_set_union(kc_set a, kc_set b);
 kc_set kc_set_intersect(kc_set a, kc_set b);
 kc_set kc_set_difference(kc_set a, kc_set b);
+kc_strset kc_strset_union(kc_strset a, kc_strset b);
+kc_strset kc_strset_intersect(kc_strset a, kc_strset b);
+kc_strset kc_strset_difference(kc_strset a, kc_strset b);
 
 typedef struct kc_set_iterator {
 	size_t index;
@@ -49,5 +61,13 @@ typedef struct kc_set_iterator {
 
 kc_set_iterator kc_set_iter(kc_set a);
 bool kc_set_next(kc_set_iterator* it, size_t* next);
+
+typedef struct kc_strset_iterator {
+	size_t index;
+	kc_strset set;
+} kc_strset_iterator;
+
+kc_strset_iterator kc_strset_iter(kc_strset a);
+bool kc_strset_next(kc_strset_iterator* it, const char* next);
 
 #endif
--- Collections/src/set.c
+++ Collections/src/set.c
@@ -4,13 +4,20 @@
 #include "kc/array.h"
 #include "kc/set.h"
 
+#define STR_HASH_LENGTH 31UL
+
 #define FNV_OFFSET 14695981039346656037UL
 #define FNV_PRIME 1099511628211UL
 
 static void _set_item(kc_set set, size_t val, bool isNew);
+static void _strset_item(kc_strset set, const char* val, bool isNew);
 
 static size_t _set_get_hash(kc_set t, size_t val) {
 	return kc_set_hash(t)(val);
+}
+
+static size_t _strset_get_hash(kc_strset t, const char* val) {
+	return kc_strset_hash(t)(val);
 }
 
 static kc_set_item* set_expand(kc_set set) {
@@ -26,10 +33,31 @@ static kc_set_item* set_expand(kc_set set) {
 	return temp.data;
 }
 
+static kc_strset_item* strset_expand(kc_strset set) {
+	size_t newCap = kc_strset_cap(set) * 2;
+	kc_strset temp = kc_strset_init(newCap);
+	for (size_t i = 0; i < kc_strset_cap(set); i++) {
+		if (set.data[i].is_used) {
+			kc_strset_set(&temp, set.data[i].item);
+		}
+	}
+
+	kc_arr_free(set.data);
+	return temp.data;
+}
+
 static void set_rehash(kc_set set, size_t start) {
 	while (set.data[start].is_used) {
 		set.data[start].is_used = false;
 		_set_item(set, set.data[start].item, false);
+		start++;
+	}
+}
+
+static void strset_rehash(kc_strset set, size_t start) {
+	while (set.data[start].is_used) {
+		set.data[start].is_used = false;
+		_strset_item(set, set.data[start].item, false);
 		start++;
 	}
 }
@@ -60,31 +88,38 @@ kc_set kc_set_init(size_t len) {
 	return set;
 }
 
-kc_strset kc_sset_init(size_t len) {
+kc_strset kc_strset_init(size_t len) {
 	kc_strset set;
 	set.data = NULL;
 	kc_arr_setcap(set.data, len);
 	kc_arr_header(set.data)->str_hash = kc_str_hash;
-	memset(set.data, 0, sizeof(kc_set_item) * len);
+	memset(set.data, 0, sizeof(kc_strset_item) * len);
 	return set;
 }
 
 void kc_set_free(kc_set t) {
 	kc_arr_free(t.data);
 }
-
 size_t kc_set_len(kc_set t) {
 	return kc_arr_len(t.data);
 }
-
 size_t kc_set_cap(kc_set t) {
+	return kc_arr_cap(t.data);
+}
+
+void kc_strset_free(kc_strset t) {
+	kc_arr_free(t.data);
+}
+size_t kc_strset_len(kc_strset t) {
+	return kc_arr_len(t.data);
+}
+size_t kc_strset_cap(kc_strset t) {
 	return kc_arr_cap(t.data);
 }
 
 int_hash kc_set_hash(kc_set t) {
 	return kc_arr_header(t.data)->hash;
 }
-
 str_hash kc_strset_hash(kc_strset t) {
 	return kc_arr_header(t.data)->str_hash;
 }
@@ -95,7 +130,7 @@ bool kc_set_has(kc_set set, size_t val) {
 		if (set.data[i].item == val) {
 			return true;
 		}
-		i++;
+		i = (i + 1 >= kc_set_cap(set)) ? 0 : (i + 1);
 	}
 	return false;
 }
@@ -219,6 +254,141 @@ bool kc_set_next(kc_set_iterator* it, size_t* next) {
 
 	if (it->index < kc_set_cap(it->set)) {
 		*next = it->set.data[it->index++].item;
+		return true;
+	}
+	return false;
+}
+
+bool kc_strset_has(kc_strset set, const char* val) {
+	size_t i = _strset_get_hash(set, val) % kc_strset_cap(set);
+	while (set.data[i].is_used) {
+		if (!strncmp(set.data[i].item, val, 31)) {
+			return true;
+		}
+		i = (i + 1 >= kc_strset_cap(set)) ? 0 : (i + 1);
+	}
+	return false;
+}
+
+static void _strset_item(kc_strset set, const char* val, bool isNew) {
+	size_t i = _strset_get_hash(set, val) % kc_strset_cap(set);
+	kc_strset_item new = set.data[i];
+	if(!new.is_used || (new.is_used && new.item == val)) {
+		if (isNew && !new.is_used) {
+			kc_arr_len(set.data)++;
+		}
+
+		new.is_used = true;
+		strncpy(set.data[i].item, val, STR_HASH_LENGTH);
+		set.data[i] = new;
+	} else {
+		while (set.data[i].is_used && set.data[i].item != val) {
+			i++;
+			if (i >= kc_strset_cap(set)) {
+				i = 0;
+			}
+		}
+
+		if (isNew && !set.data[i].is_used) {
+			kc_arr_len(set.data)++;
+		}
+		set.data[i].is_used = true;
+		strncpy(set.data[i].item, val, STR_HASH_LENGTH);
+	}
+}
+
+void kc_strset_set(kc_strset * set, const char* val) {
+	if (kc_strset_len(*set) > kc_strset_cap(*set) / 2) {
+		set->data = strset_expand(*set);
+	}
+	_strset_item(*set, val, true);
+}
+void kc_strset_remove(kc_strset set, const char* val) {
+	size_t cap = kc_strset_cap(set);
+	size_t i = _strset_get_hash(set, val) % cap;
+	while(set.data[i].is_used && strncmp(set.data[i].item, val, STR_HASH_LENGTH)) {
+		i++;
+	}
+	
+	if (set.data[i].is_used && !strncmp(set.data[i].item, val, STR_HASH_LENGTH)) {
+		set.data[i].is_used = false;
+		kc_arr_len(set.data)--;
+		size_t start = (i + 1) % cap;
+		strset_rehash(set, start);
+	}
+}
+
+kc_strset kc_strset_union(kc_strset a, kc_strset b) {
+	kc_strset un = kc_strset_init(kc_strset_cap(a) + kc_strset_cap(b));
+	kc_strset_iterator it = kc_strset_iter(a);
+	const char* val;
+	while (kc_strset_next(&it, val)) {
+		kc_strset_set(&un, val);
+	}
+
+	it = kc_strset_iter(b);
+	while (kc_strset_next(&it, val)) {
+		kc_strset_set(&un, val);
+	}
+
+	return un;
+}
+
+kc_strset kc_strset_intersect(kc_strset a, kc_strset b) {
+	size_t ca = kc_strset_cap(a);
+	size_t cb = kc_strset_cap(b);
+	kc_strset in = kc_strset_init(ca < cb ? cb : ca);
+
+	kc_strset m = ca < cb ? a : b;
+	kc_strset o = ca < cb ? b : a;
+	kc_strset_iterator it = kc_strset_iter(m);
+	const char* val;
+	while (kc_strset_next(&it, val)) {
+		if (kc_strset_has(o, val)) {
+			kc_strset_set(&in, val);
+		}
+	}
+
+	return in;
+}
+
+kc_strset kc_strset_difference(kc_strset a, kc_strset b) {
+	size_t ca = kc_strset_cap(a);
+	size_t cb = kc_strset_cap(b);
+	kc_strset diff = kc_strset_init(ca + cb);
+
+	const char* val;
+	kc_strset_iterator it = kc_strset_iter(a);
+	while (kc_strset_next(&it, val)) {
+		if (!kc_strset_has(b, val)) {
+			kc_strset_set(&diff, val);
+		}
+	}
+
+	it = kc_strset_iter(b);
+	while (kc_strset_next(&it, val)) {
+		if (!kc_strset_has(a, val)) {
+			kc_strset_set(&diff, val);
+		}
+	}
+
+	return diff;
+}
+
+kc_strset_iterator kc_strset_iter(kc_strset a) {
+	kc_strset_iterator it;
+	it.index = 0;
+	it.set = a;
+	return it;
+}
+
+bool kc_strset_next(kc_strset_iterator* it, const char* next) {
+	while (it->index < kc_strset_cap(it->set) && !it->set.data[it->index].is_used) {
+		it->index++;
+	}
+
+	if (it->index < kc_strset_cap(it->set)) {
+		next = it->set.data[it->index].item;
 		return true;
 	}
 	return false;
